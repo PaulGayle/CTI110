d139a6e485ea868b848c1d527d4cee2d2e26ef76
PaulGayle <159733708+PaulGayle@users.noreply.github.com>
2024-02-11 18:08:40 -0500

    Add files via upload
    First submission. A first step towards reaching my goals.

---
+++ P1LAB1_GaylePaul.py
@@ -0,0 +1,12 @@
+#Name: PaulGayle
+#Date: 02/10/2024
+#Assignment:P1LAB1_GaylePaul
+#Description of Assignment: This assignment is simply showing the usage of input, print, and output functions.
+
+name=input("Enter your name. ")
+
+print("Hello "+ name )
+print("We are happy to welcome you to Zybooks!")
+
+
+
