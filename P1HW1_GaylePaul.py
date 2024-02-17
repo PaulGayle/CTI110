99df35ec0c745a66bfad07912e0b34fec72b55ea
PaulGayle <159733708+PaulGayle@users.noreply.github.com>
2024-02-17 14:22:51 -0500

    Add files via upload

---
+++ P1HW1_GaylePaul.py
@@ -0,0 +1,19 @@
+#Name: Paul Gayle
+#Date: 2/14/2024
+#Assignment:P1HW1
+#Description: This assignment will test code that collects and processes recieved information.
+
+x=input("enter integer. ")
+squared=int(x)**2
+cubed=int(x)**3
+
+print("You entered " + x)
+print(str(x) + " squared is " + str(squared))
+print("And " + str(x) + " Cubed is "+ str(cubed))
+y=input("Enter another integer ")
+z=int(x)+ int(y)
+z1=int(x)* int(y)
+print(str(x) +" plus "+ str(y) +" equals "+ str(z))
+print(str(x) +" multiplied by " + str(y)+" equals "+ str(z1))
+
+
