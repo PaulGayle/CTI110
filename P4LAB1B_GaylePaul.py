f7048c75a81e10fc80337c275de6ca1882b2ac96
PaulGayle <159733708+PaulGayle@users.noreply.github.com>
2024-04-19 23:29:20 -0400

    Add files via upload

---
+++ P4LAB1B_GaylePaul.py
@@ -0,0 +1,28 @@
+#Name: Paul Gayle
+#Date: 04/13/2024
+#Assignment: P4LAB1B
+#Description: Assignment shows student's basic knowlege about turtle graphics
+
+import turtle as tim
+tim.shape("turtle")
+tim.color("red")
+tim.penup()
+tim.goto(-50, 0)
+tim.pendown()
+
+tim.left(90)
+tim.forward(100)
+tim.circle(-50, 180)
+tim.circle(-50, 180)
+
+tim.penup()
+tim.right(90)
+tim.forward(225)
+tim.left(90)
+tim.pendown()
+tim.circle(50)
+tim.left(180)
+tim.forward(75)
+tim.circle(-50, 180)
+
+
